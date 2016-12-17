aaf518fe03cc176f921e220fe3b2c8baabb640eb
vinicius <VSSilva233>
2016-12-17 00:39:33 -0200

    Tentantiva de utilizar camera kk

--- app/src/main/java/com/example/vinicius/desafiojera/activity/RegisterLivroActivity.java
+++ app/src/main/java/com/example/vinicius/desafiojera/activity/RegisterLivroActivity.java
@@ -1,6 +1,9 @@
 package com.example.vinicius.desafiojera.activity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +18,8 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class RegisterLivroActivity extends AppCompatActivity {
+    //Imagem
+    private static final int CAMERA_REQUEST = 123;
     //Utilizando ButterKnife para injeção de Views
     @BindView(R.id.img_livro)
     ImageView imagemLivro;
@@ -29,6 +34,26 @@ public class RegisterLivroActivity extends AppCompatActivity {
 
     Livro livro;
 
+    //Imagem
+    @OnClick(R.id.img_livro)
+    public void changeImage() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 123 && resultCode == RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imagemLivro.setImageBitmap(photo);
+        } else {
+            Toast.makeText(this, "Imagem não carregada", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
     //Chamando/criando layout activity registro(cadastro) de livros
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +61,7 @@ public class RegisterLivroActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register_livro);
         ButterKnife.bind(this);
     }
-
+     //
     @OnClick(R.id.btn_salvar_livro)
     public void salvar(Button button) {
         String name = editNomeLivro.getText().toString();
